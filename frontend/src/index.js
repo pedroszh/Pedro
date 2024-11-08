482e71abf7fd95539a93bfc9c9224b7a1f2cc61d
pedroszh <pedrinho.sousasantos34@gmail.com>
2024-11-08 09:07:01 -0300

    resolvendo conflitos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';  // Se houver estilos globais
+import App from './App';  // O componente principal
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')  // Aqui o React será montado
 );
